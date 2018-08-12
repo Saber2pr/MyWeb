94a14163d5e2858563a17105f165b54dae5d9e07
AK-12 <saber2pr@gmail.com>
2018-08-12 23:19:34 +0800

    add BlogHtml

---
+++ js/input.js
@@ -0,0 +1,19 @@
+function submit(obj) {
+
+    if (obj.value === 'Input you words here...') {
+
+        alert("你还没输入呢！");
+
+    }else {
+        console.log(obj.value);
+        alert("AK-12:抱歉!数据库功能正在完善中~");
+
+    }
+
+}
+
+
+
+
+
+
