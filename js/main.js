8d96c60f9e8217d187157aed449f259178a615c1
AK-12 <saber2pr@gmail.com>
2018-08-16 22:36:28 +0800

    resetBoot&jq

--- js/main.js
+++ js/main.js
@@ -4,10 +4,6 @@ require.config({
 
     paths: {
 
-        jquery: 'https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js',
-
-        bootstrap: 'https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js',
-
         sql: 'plugin/sql',
 
         sqlTool: 'database/sqlTool',
@@ -19,12 +15,6 @@ require.config({
     },
 
     shim: {
-
-        bootstrap: {
-
-            deps: ['jquery']
-
-        },
 
         database: {
 
