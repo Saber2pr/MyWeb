94aa2242126c6f10058220bdbf673307bb773c06
saber2pr <saber2pr@gmail.com>
2018-11-16 21:29:31 +0800

    ad

--- build/EdgeJudge/src/settings.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],rawAssets:{assets:{}},assetTypes:[],launchScene:"db://assets/main.fire",scenes:[{url:"db://assets/main.fire",uuid:0}],packedAssets:{"0e9a33ee9":["a2MjXRFdtLlYQ5ouAFv/+R",0]},md5AssetsMap:{},orientation:"",subpackages:{},uuids:["b4HxNI5cJH85X+cza0btWV"]};
